1f4e8d13bb8c15c8788552fd07b7e07e1e2d218b
htbkoo <h1017760@connect.hku.hk>
2018-06-11 23:07:33 +0800

    updated src\jsBasics\basics\one\2_js_more_types.js htbkoo/intro-to-js-react

--- src/jsBasics/basics/one/2_js_more_types.js
+++ src/jsBasics/basics/one/2_js_more_types.js
@@ -2,12 +2,28 @@ var arrayInJS = [1, 2, 3, "we can mix type in JS"];
 console.log(arrayInJS);
 console.log("we can access the item in an array like this: " + arrayInJS[1]);
 
+arrayInJS[0]=4;
+
 var objectInJS = {
     "object consists of a key": "and a value",
     "thisShouldLookFamilarToYouIfYouKnow": "JSON",
-    1: 2
+    "1": 2,
+    "another object":{
+        "nesting is OK":[
+            1
+        ]
+    }
 };
 console.log(objectInJS);
+console.log(objectInJS["object consists of a key"]);
+console.log(objectInJS["another object"]);
+console.log(objectInJS["another object"]["nesting is OK"]);
+
+objectInJS["1"]=3;
+objectInJS["2"]=4;
+
+delete objectInJS["1"];
+
 console.log(JSON.stringify(objectInJS));
 
 var date = new Date();
