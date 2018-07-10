6deb0400f57526a34795e368ff4b861ce9b3e776
htbkoo <h1017760@connect.hku.hk>
2018-07-10 08:11:22 +0800

    added more exercises in 1_js_basics.test.js htbkoo/intro-to-js-react

--- src/exercises/1_js_basics.test.js
+++ src/exercises/1_js_basics.test.js
@@ -3,37 +3,71 @@ var chai = require("chai");
 describe("Some JS basic exercises", function () {
     // These tests does not pass, try fixing them
 
-    it("should assert sum", function () {
-        // given
-        // when
-        var sum = 2 + 2;
+    describe("basic syntax", function () {
+        it("should assert sum", function () {
+            // given
+            // when
+            var sum = 2 + 2;
 
-        // then
-        // TODO: fix this
-        chai.expect(sum).to.equal(5);
+            // then
+            // TODO: fix this
+            chai.expect(sum).to.equal(5);
+        });
+
+        it("should get the type", function () {
+            // then
+            // TODO: fix these
+            chai.expect(typeof 5).to.equal();
+            chai.expect(typeof "").to.equal();
+            chai.expect(typeof function () {
+            }).to.equal();
+            chai.expect(typeof true).to.equal();
+            chai.expect(typeof {}).to.equal();
+            chai.expect(typeof []).to.equal();
+        });
     });
 
-    it("should get 0,1,2,3,4 from fn()", function () {
-        // given
-        var fn = [];
+    describe("array", function () {
+        it("should get the value from array", function () {
+            var array = [0, 1, 2, 3];
 
-        // when
-        // TODO: fix this
-        for (var i = 0; i < 5; i++) {
-            fn.push(function () {
-                return i;
-            })
-        }
+            // then
+            // TODO: fix this
+            chai.expect(array[replaceThis]).to.equal(1);
+        });
+    });
 
-        // then
-        [
-            0,
-            1,
-            2,
-            3,
-            4,
-        ].forEach(function (expected, index) {
-            chai.expect(fn[index]()).to.equal(expected);
+    describe("object", function () {
+        it("should get the value from object", function () {
+            var object = {
+                anotherKey: "not this",
+                someKey: "someValue",
+                1: "nor this"
+            };
+
+            // then
+            // TODO: fix this
+            chai.expect(object[replaceThis]).to.equal("someValue");
+        });
+
+        it("should assign value to object", function () {
+            var object = {};
+            // TODO: fix this
+            object;
+
+            // then
+            chai.expect(object["key"]).to.equal("someValue");
+        });
+
+        it("should remove value from object", function () {
+            var object = {
+                key: "should be removed"
+            };
+            // TODO: fix this
+            object;
+
+            // then
+            chai.expect(Object.keys(object).length).to.equal(0);
         });
     });
 });
