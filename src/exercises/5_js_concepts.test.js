9d6757dbc60cf95273f16c03f7dc018803646b67
htbkoo <h1017760@connect.hku.hk>
2018-07-17 08:20:31 +0800

    grouped exercises htbkoo/intro-to-js-react

--- src/exercises/5_js_concepts.test.js
+++ src/exercises/5_js_concepts.test.js
@@ -2,41 +2,46 @@ var chai = require("chai");
 
 describe("JS concepts", function () {
     // These tests do not pass, try fixing them
-    it("should get 0,1,2,3,4 from fn()", function () {
-        // given
-        var fn = [];
 
-        // when
-        // TODO: fix this
-        for (var i = 0; i < 5; i++) {
-            fn.push(function () {
-                return i;
-            })
-        }
+    describe("lexical scoping", function () {
+        it("should get 0,1,2,3,4 from fn()", function () {
+            // given
+            var fn = [];
 
-        // then
-        [
-            0,
-            1,
-            2,
-            3,
-            4,
-        ].forEach(function (expected, index) {
-            chai.expect(fn[index]()).to.equal(expected);
+            // when
+            // TODO: fix this
+            for (var i = 0; i < 5; i++) {
+                fn.push(function () {
+                    return i;
+                })
+            }
+
+            // then
+            [
+                0,
+                1,
+                2,
+                3,
+                4,
+            ].forEach(function (expected, index) {
+                chai.expect(fn[index]()).to.equal(expected);
+            });
         });
     });
 
-    it("seems that 'this' does not work", function () {
-        // given
-        var getHead = function () {
-            return this[0];
-        };
+    describe("this", function () {
+        it("seems that 'this' does not work", function () {
+            // given
+            var getHead = function () {
+                return this[0];
+            };
 
-        // when
-        var context = ["someSecret", 2, 3];
+            // when
+            var context = ["someSecret", 2, 3];
 
-        // then
-        // TODO: fix this
-        chai.expect(replaceThis).to.equal("someSecret");
+            // then
+            // TODO: fix this
+            chai.expect(replaceThis).to.equal("someSecret");
+        });
     });
 });
