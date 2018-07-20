b2d108656d30e5cb74441601740639b5fc1ac453
htbkoo <h1017760@connect.hku.hk>
2018-07-20 08:29:13 +0800

    added hints htbkoo/intro-to-js-react

--- src/exercises/5_js_concepts.test.js
+++ src/exercises/5_js_concepts.test.js
@@ -2,8 +2,11 @@ var chai = require("chai");
 
 describe("JS concepts", function () {
     // These tests do not pass, try fixing them
+    // Hint: There are "Hints" on top of the test cases which link to the corresponding MDN docs, take a look if needed!
 
+    // Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures#Lexical_scoping
     describe("lexical scoping", function () {
+        // Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures#Creating_closures_in_loops_A_common_mistake
         it("should get 0,1,2,3,4 from fn()", function () {
             // given
             var fn = [];
@@ -29,6 +32,7 @@ describe("JS concepts", function () {
         });
     });
 
+    // Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this
     describe("this", function () {
         it("seems that 'this' does not work", function () {
             // given
