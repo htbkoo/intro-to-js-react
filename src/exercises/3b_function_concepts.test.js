53a82f8c682d071ea1ed86cee5fff96c39334eca
htbkoo <h1017760@connect.hku.hk>
2018-07-20 08:48:39 +0800

    added exercise and hints htbkoo/intro-to-js-react

--- src/exercises/3b_function_concepts.test.js
+++ src/exercises/3b_function_concepts.test.js
@@ -2,6 +2,7 @@ var chai = require("chai");
 
 describe("JS functions (more advanced concepts)", function () {
     // These tests do not pass, try fixing them
+    // Hint: There are "Hints" on top of the test cases which link to the corresponding MDN docs, take a look if needed!
 
     describe("function as first class object", function () {
         it("should pass callback function as an argument", function () {
@@ -18,6 +19,29 @@ describe("JS functions (more advanced concepts)", function () {
 
             // then
             chai.expect(fn(options, callback)).to.equal("callback return value");
+        });
+    });
+
+    // Hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
+    describe("new", function () {
+        it("should create new instance", function () {
+            // given
+            function MyClass(value) {
+                var _value = value;
+                this.publicValue = value + 5;
+                this.getValue = function () {
+                    return _value;
+                };
+                return this;
+            }
+
+            // when
+            var myClass = new MyClass(10);
+
+            // then
+            // TODO: fix this
+            chai.expect(myClass.getValue()).to.equal(replaceThis);
+            chai.expect(myClass.publicValue).to.equal(replaceThis);
         });
     });
 });
